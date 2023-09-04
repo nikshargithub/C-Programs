d5537d6935b1b07912c70d5c2ba11a2a6d74e19a
nikshargithub <143412005+nikshargithub@users.noreply.github.com>
2023-09-04 16:33:35 +0530

    Delete Lab1 directory

--- Lab1/area of circle.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int l,ar;
-	printf("Enter radius of circle:");
-	scanf("%d",&l);
-	ar = 3.14*l*l;
-	printf("Area of circle:%d",ar);
-}
--- Lab1/cube of number.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a;
-	printf("Enter a number:");
-	scanf("%d",&a);
-	printf("cube of number:%d",a*a*a);
-	return 0;
-}
